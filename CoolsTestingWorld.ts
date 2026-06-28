204c69ae4edd4844708ad56817c7b5d15834e838
coolconstruction12 <294031203+coolconstruction12@users.noreply.github.com>
2026-06-28 13:34:38 +1200

    Modify console logs and add walls in CoolsTestingWorld
    Updated console messages and added two walls to the world.

--- CoolsTestingWorld.ts
+++ CoolsTestingWorld.ts
@@ -9,18 +9,40 @@ import { Color } from "./Yuu API/Basic Types/Color";
 registerStart(start);
 function start() {
   inWorldConsole.visible(true, new Vector3(0, 1.5, -1.5));
-  console.log('Hi and Welcome to Cools Test World V0.1!', Color.blue);
-  console.log('Last change was: "changed the both console colours"', Color.red);
+  console.log('Hi and Welcome to Cools Test World V0.1!');
+  console.log('Last change was: " added two walls to the world"');
 
   startGame();
 }
 
 function startGame() {
-  const cube =spawnPrimitive.cube(
+  const cubefloor =spawnPrimitive.cube(
     new Vector3(0, 0, -25),
     new Vector3(20, 0.2, 43),
     Quaternion.one,
     Color.green,
+    1,
+    true,
+    'Static',
+    undefined,
+  );
+
+   const cubeleftwall =spawnPrimitive.cube(
+    new Vector3(-21.5, 3, -25),
+    new Vector3(0.2, 3, 43),
+    Quaternion.one,
+    Color.blue,
+    1,
+    true,
+    'Static',
+    undefined,
+  );
+
+   const cuberightwall =spawnPrimitive.cube(
+    new Vector3(21.5, 3, -25),
+    new Vector3(0.2, 3, 43),
+    Quaternion.one,
+    Color.red,
     1,
     true,
     'Static',

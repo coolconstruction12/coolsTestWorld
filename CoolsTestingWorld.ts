2d7a6067d189edfbb9e24bbf1e8963f57c51d93c
coolconstruction12 <294031203+coolconstruction12@users.noreply.github.com>
2026-06-28 12:31:18 +1200

    Implement cube spawning in startGame function
    Added a function to spawn a cube in the game world.

--- CoolsTestingWorld.ts
+++ CoolsTestingWorld.ts
@@ -1,6 +1,9 @@
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { inWorldConsole } from "./Yuu API/Console";
 import { registerStart } from "./Yuu API/RegisterStart";
+import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
+import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
+import { Color } from "./Yuu API/Basic Types/Color";
 
 
 registerStart(start);
@@ -8,4 +11,20 @@ function start() {
   inWorldConsole.visible(true, new Vector3(0, 1.5, -1.5));
 
   console.log('Hi and Welcome to Cools Test World V0.1!');
+
+  startGame();
+}
+
+function startGame() {
+  const cube =spawnPrimitive.cube(
+    new Vector3(0, 0, -5),
+    new Vector3(8, 0.2, 8),
+    Quaternion.one,
+    Color.green,
+    1,
+    true,
+    'Static',
+    undefined,
+  );
+
 }

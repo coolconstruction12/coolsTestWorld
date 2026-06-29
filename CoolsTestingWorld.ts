bb209891846b5564f1afe301226ee3420fc3785f
coolconstruction12 <294031203+coolconstruction12@users.noreply.github.com>
2026-06-29 16:01:56 +1200

    Refactor maze building functionality

--- CoolsTestingWorld.ts
+++ CoolsTestingWorld.ts
@@ -1,27 +1,24 @@
 import { Vector3 } from "./Yuu API/Basic Types/Vector3";
 import { inWorldConsole } from "./Yuu API/Console";
 import { registerStart } from "./Yuu API/RegisterStart";
-import { spawnPrimitive } from "./Yuu API/SpawnPrimitive";
-import { Quaternion } from "./Yuu API/Basic Types/Quaternion";
-import { Color } from "./Yuu API/Basic Types/Color";
+import { buildMaze } from "./CoolsMaze";
 
 
 registerStart(start);
 function start() {
   inWorldConsole.visible(true, new Vector3(0, 1.5, -1.5));
   console.log('Hi and Welcome to Cools Test World V0.1!');
-  console.log('Last change was: " moved the walls to the right position"');
+  console.log('Last change was: " working on the maze walls"');
 
   
   function startGamelevels() {
-   buildMaze();
+   Mazelevel();
 
   }
 
 
-  function buildMaze() {
-
+  function Mazelevel() {
+    buildMaze.spawnMaze();
 }
-
 
 }
